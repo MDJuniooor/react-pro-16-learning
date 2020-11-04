ee474228f1b0c7f76594d60caa87e80cba456b53
mdjuniooor <mmdsds@u.sogang.ac.kr>
2020-11-05 08:59:28 +0900

    Learn Async && Promise Concept

--- primer/src/index.js
+++ primer/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import "./example"
+
 ReactDOM.render(<App />,  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
