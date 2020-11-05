f4e57e831edf67c39a6596859988e0b84dd8955f
mdjuniooor <mmdsds@u.sogang.ac.kr>
2020-11-05 17:45:54 +0900

    Attach Data Using REST API

--- sportsstore/src/data/DataStore.js
+++ sportsstore/src/data/DataStore.js
@@ -1,7 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { ShopReducer } from "./ShopReducer";
 import { CartReducer } from "./CartReducer";
 import { CommonReducer } from "./CommonReducer";
+import { asyncActions } from "./AsyncMiddleware";
 
+// 미들웨어를 래핑해 액션을 받으며, 그 결과를 데이터 스토어를 생성하는 createStore 함수에 인자로 전달.
 export const SportsStoreDataStore 
-    = createStore(CommonReducer(ShopReducer, CartReducer));
+    = createStore(CommonReducer(ShopReducer, CartReducer), 
+      applyMiddleware(asyncActions)); 
