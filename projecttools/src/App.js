1647255c966557ec5530fadbf0fda33cd6bf4471
mdjuniooor <mmdsds@u.sogang.ac.kr>
2021-01-17 10:52:43 +0900

    Learn React props Change

--- projecttools/src/App.js
+++ projecttools/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Summary } from "./Summary";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let names = ["Bob", "Alice", "Dora"]
+
+function reverseNames() {
+  names.reverse();
+  ReactDOM.render(<App />, document.getElementById('root'));
 }
 
-export default App;
+function promoteName(name) {
+  names = [name, ...names.filter(val => val !== name)];
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = (increment) => setCounter(counter + increment);
+
+  return (
+    <table className="table table-sm table-striped">
+      <thead>
+        <tr><th>#</th><th>Name</th><th>Letters</th></tr>
+      </thead>
+      <tbody>
+        { names.map((name, index) =>
+          <tr key={ name }>
+            <Summary index={index} name={name}
+              reverseCallback={reverseNames}
+              promoteCallback={promoteName}
+              counter={ counter }
+              incrementCallback={ incrementCounter }
+            />
+          </tr>
+        )}
+      </tbody>
+    </table>
+  )
+}
